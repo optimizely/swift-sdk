8fa584cf61e8527d52bca3003f21983c2c0a170c
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-02-28 15:57:33 -0800

    add valid json files from compat suite and write unit tests for batch event builder

--- OptimizelySDK/OptimizelyTests/OptimizelyTests-Common-iOS/BatchEventBuilderTest.swift
+++ OptimizelySDK/OptimizelyTests/OptimizelyTests-Common-iOS/BatchEventBuilderTest.swift
@@ -10,9 +10,9 @@ import XCTest
 
 class BatchEventBuilderTest: XCTestCase {
     
-    let datafileName = "valid-project-config-v4"
-    let basicExperimentKey = "basic_experiment"
-    let eventWithNoExperimentKey = "event_with_no_experiment"
+    let datafileName = "feature_experiments"
+    let featureExperimentKey = "feature_targeted_exp"
+    let eventWithNoExperimentKey = "unused_event"
     let userId = "userId"
     var optimizely:OptimizelyManager?
     
@@ -31,15 +31,38 @@ class BatchEventBuilderTest: XCTestCase {
     }
 
     override func tearDown() {
+        optimizely = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testConversionEventWithNoExperiment() {
         let conversion = BatchEventBuilder.createConversionEvent(config: (optimizely?.config)!, decisionService: (optimizely?.decisionService)!, eventKey: eventWithNoExperimentKey, userId: userId, attributes: ["anyattribute":"value", "broswer_type":"firefox"], eventTags: nil)
         
         XCTAssertNotNil(conversion)
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let batchEvent = try? JSONDecoder().decode(BatchEvent.self, from: conversion!)
+        
+        XCTAssertNotNil(batchEvent)
+        
+        XCTAssert((batchEvent?.enrichDecisions)! == true)
+        
+    }
+
+    func testImpressionEventWithNoExperiment() {
+        let experiment = optimizely?.config.project.experiments.filter({$0.key == featureExperimentKey}).first
+        let variation = experiment?.variations[0]
+        
+        let impression = BatchEventBuilder.createImpressionEvent(config: (optimizely?.config)!, decisionService: (optimizely?.decisionService)!, experiment: experiment!, varionation: variation!, userId: userId, attributes: ["customattr": "yes" ])
+        
+        XCTAssertNotNil(impression)
+        let batchEvent = try? JSONDecoder().decode(BatchEvent.self, from: impression!)
+        
+        XCTAssertNotNil(batchEvent)
+        
+        XCTAssert((batchEvent?.enrichDecisions)! == true)
+        
+        XCTAssert(batchEvent?.visitors[0].attributes[0].key == "customattr")
+        //XCTAssert(batchEvent?.visitors[0].attributes[0].value == .string)
     }
 
     func testPerformanceExample() {

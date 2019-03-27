4b9a9627643fabebf9dc9a47d8a69c8fb9948d2a
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-03-27 12:15:35 -0700

    add one more performAtomic

--- OptimizelySDK/Customization/DefaultEventDispatcher.swift
+++ OptimizelySDK/Customization/DefaultEventDispatcher.swift
@@ -67,7 +67,7 @@ open class DefaultEventDispatcher : BackgroundingCallbacks, OPTEventDispatcher {
     }
     
     deinit {
-        if let timer = timer.property {
+        timer.performAtomic() { (timer) in
             timer.invalidate()
         }
         unsubscribe()

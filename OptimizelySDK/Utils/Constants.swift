e4999ad3eb3d9c323c4885e6f0c0100619a63af2
Yasir Ali <>
2019-03-27 15:57:59 +0500

    Redundant keys removed.

--- OptimizelySDK/Utils/Constants.swift
+++ OptimizelySDK/Utils/Constants.swift
@@ -15,23 +15,14 @@ struct Constants {
         static let OptimizelyUserAgent = "$opt_user_agent";
     }
     struct NotificationKeys {
-        static let OptimizelyNotificationExperiment = "experiment"
-        static let OptimizelyNotificationVariation = "variation"
-        static let OptimizelyNotificationUserId = "userId"
-        static let OptimizelyNotificationAttributes = "attributes"
-        static let OptimizelyNotificationEvent = "eventKey"
-        static let OptimizelyNotificationEventTags = "eventTags"
-        static let OptimizelyNotificationLogEventParams = "logEventParams"
         static let OptimizelyNotificationDecisionInfoFeature = "featureKey"
         static let OptimizelyNotificationDecisionInfoFeatureEnabled = "featureEnabled"
-        static let OptimizelyNotificationDecisionInfo = "decisionInfo"
         static let OptimizelyNotificationDecisionInfoSourceExperiment = "sourceExperimentKey"
         static let OptimizelyNotificationDecisionInfoSourceVariation = "sourceVariationKey"
         static let OptimizelyNotificationDecisionInfoSource = "source"
         static let OptimizelyNotificationDecisionInfoVariable = "variableKey"
         static let OptimizelyNotificationDecisionInfoVariableType = "variableType"
         static let OptimizelyNotificationDecisionInfoVariableValue = "variableValue"
-        static let OptimizelyNotificationDecisionType = "type"
         static let OptimizelyDecisionTypeFeatureVariable  = "feature_variable"
     }
     struct DecisionSource {

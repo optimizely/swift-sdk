d5bf30757435158cd4f5a3a907edeb85f09af378
Yasir Ali <yali@folio3.com>
2019-04-03 14:09:05 +0500

    Recommended changes made.

--- OptimizelySDK/Optimizely/OptimizelyManager.swift
+++ OptimizelySDK/Optimizely/OptimizelyManager.swift
@@ -397,10 +397,7 @@ open class OptimizelyManager: NSObject {
         // fix DecisionService to throw error
         let pair = decisionService.getVariationForFeature(config: config, featureFlag: featureFlag, userId: userId, attributes: attributes ?? OptimizelyAttributes())
         
-        var args = Array<Any?>()
-        args.append(Constants.DecisionTypeKeys.isFeatureEnabled)
-        args.append(userId)
-        args.append(attributes ?? OptimizelyAttributes())
+        var args: Array<Any?> = (self.notificationCenter as! DefaultNotificationCenter).getArgumentsForDecisionListener(notificationType: Constants.DecisionTypeKeys.isFeatureEnabled, userId: userId, attributes: attributes)
         
         var decisionInfo = Dictionary<String,Any>()
         decisionInfo[Constants.DecisionInfoKeys.feature] = featureKey

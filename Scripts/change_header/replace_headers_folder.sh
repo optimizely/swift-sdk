a339f70e075856bdfc7b1997e62b421b89d3b2e2
Jae Kim <jae.kim@optimizely.com>
2019-04-26 12:58:44 -0700

    cleanup

--- Scripts/change_header/replace_headers_folder.sh
+++ Scripts/change_header/replace_headers_folder.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-SCRIPT_DIR=`dirname $0`
 if [ $# != 1 ]; then
     echo "Usage: replace_headers_folder.sh <folder>"
     exit 1
 fi
 
+SCRIPT_DIR=`dirname $0`
 ROOT_DIR="${PWD}/$1"
 
 # do no break spaces in file names
 IFS=$'\n'
+
+pushd $SCRIPT_DIR > /dev/null
 
 for f in $(find $ROOT_DIR -name '*.swift' -or -name '*.h' -or -name '*.m')
 do
@@ -16,9 +18,8 @@ do
         continue
     fi
 
-    pushd $SCRIPT_DIR > /dev/null
     echo "replacing header for: $f"
     replace_header.sh "$f"
-    popd > /dev/null
 done
 
+popd > /dev/null

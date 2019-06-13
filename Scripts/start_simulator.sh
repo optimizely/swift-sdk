9f95ffc617cdbc51dc59ff5f13de7a6c02b49185
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-06-13 14:58:00 -0700

    test

---
+++ Scripts/start_simulator.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+# prep jq arg because it doesnt allow parameter expansion within its single quotes
+echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${OS/./-}\"" > /tmp/jq_file
+
+xcrun simctl boot $( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' ) && sleep 30
+xcrun simctl list | grep Booted

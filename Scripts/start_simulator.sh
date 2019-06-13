09da1ba570a4014685ab209d5c3d153fc6e95b23
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-06-13 10:03:56 -0700

    test

--- Scripts/start_simulator.sh
+++ Scripts/start_simulator.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -eou pipefail 
+set -eou pipefail
+
 echo "PLATFORM: $PLATFORM OS: $OS NAME: $NAME"
-echo "first"
-xcrun simctl list --json | jq '.devices."'"$(echo $PLATFORM | awk '{print $1}') $OS"'"'
-echo "second"
-xcrun simctl boot $(xcrun simctl list --json | jq '.devices."'"$(echo $PLATFORM | awk '{print $1}') $OS"'"' | jq -r '.[] | select(.name==env.NAME) | .udid')
+xcrun simctl boot $(xcrun simctl list --json | jq '.devices."'"${PLATFORM% Simulator} $OS"'"' | jq -r '.[] | select(.name==env.NAME) | .udid')
+xcrun simctl list | grep Booted

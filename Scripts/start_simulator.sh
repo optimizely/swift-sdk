d74b3ce4d2fe6ad9fc96467dee7e74ae0cc70f85
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-06-13 12:57:40 -0700

    test

--- Scripts/start_simulator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eou pipefail
-
-echo "PLATFORM: $PLATFORM OS: $OS NAME: $NAME"
-xcrun simctl boot $(xcrun simctl list --json | jq '.devices."'"${PLATFORM% Simulator} $OS"'"' | jq -r '.[] | select(.name==env.NAME) | .udid')
-xcrun simctl list | grep Booted

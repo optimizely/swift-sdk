44f103d35b85e34fed441798586a1c35504148ee
Brandon <brandon.david@optimizely.com>
2019-06-19 17:27:08 -0700

    add static ui test

--- DemoSwiftApp/DemoSwiftiOSUITests/StaticUITests.swift
+++ DemoSwiftApp/DemoSwiftiOSUITests/StaticUITests.swift
@@ -1,43 +1,43 @@
 //
 /****************************************************************************
-* Copyright 2019, Optimizely, Inc. and contributors                        *
-*                                                                          *
-* Licensed under the Apache License, Version 2.0 (the "License");          *
-* you may not use this file except in compliance with the License.         *
-* You may obtain a copy of the License at                                  *
-*                                                                          *
-*    http://www.apache.org/licenses/LICENSE-2.0                            *
-*                                                                          *
-* Unless required by applicable law or agreed to in writing, software      *
-* distributed under the License is distributed on an "AS IS" BASIS,        *
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
-* See the License for the specific language governing permissions and      *
-* limitations under the License.                                           *
-***************************************************************************/
-    
+ * Copyright 2019, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
 
 import XCTest
 
-class DemoSwiftiOSUITests: XCTestCase {
+class StaticUITests: XCTestCase {
     
     var app: XCUIApplication!
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app = XCUIApplication()
         
         // To disable animations during UI Test, uncomment line below.
         // app.launchEnvironment = ["UITEST_DISABLE_ANIMATIONS" : "YES"]
         app.launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
@@ -67,9 +67,9 @@ class DemoSwiftiOSUITests: XCTestCase {
             XCTAssertFalse(A_Text.exists)
         }
     }
-
+    
     func testConversion() {
-        // Tests if Test Conversion button works and variation persists.
+        // Tests if Test Conversion button works and variation stays the same.
         
         // given
         let variationText = app.staticTexts["VARIATION"]
@@ -102,7 +102,7 @@ class DemoSwiftiOSUITests: XCTestCase {
         let backButton = app.buttons["BACK"]
         let conversionElement = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element
         let conversionTextView = conversionElement.children(matching: .textView).element
-  
+        
         // then
         XCTAssertTrue(backButton.exists)
         XCTAssertTrue(conversionElement.exists)
@@ -124,6 +124,8 @@ class DemoSwiftiOSUITests: XCTestCase {
     }
     
     func testVariationPersistsInBackground() {
+        // Tests if variation stays the same when app goes to background and then to foreground.
+        
         // given
         let A_Text = app.staticTexts["A"]
         let B_Text = app.staticTexts["B"]
@@ -152,25 +154,5 @@ class DemoSwiftiOSUITests: XCTestCase {
             XCTAssertFalse(A_Text.exists)
         }
     }
-    
-    func testFlushBackground() {
-        let staticLabel = app.staticTexts["Current # of Optimizely events:"]
-        let testConversionButton = app.buttons["TEST CONVERSION"]
-        let backButton = app.buttons["BACK"]
-        XCTAssertTrue(staticLabel.exists)
-        
-        // Perform one conversion
-        testConversionButton.tap()
-        backButton.tap()
-        
-        // Background and foreground
-        XCUIDevice.shared.press(XCUIDevice.Button.home)
-        app.activate()
-        
-        // async check if # of events flushes to 0.
-        let zeroLabel = app.staticTexts["0"]
-        let exists = NSPredicate(format: "exists == 1")
-        expectation(for: exists, evaluatedWith: zeroLabel, handler: nil)
-        waitForExpectations(timeout: 5, handler: nil)
-    }
 }
+

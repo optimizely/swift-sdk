5dca7251dcc00a48af0e4e41169fe33535e4485f
Jae Kim <jae.kim@optimizely.com>
2019-11-06 14:54:49 -0800

    test xcode version in travis

--- Scripts/test_all.sh
+++ Scripts/test_all.sh
@@ -5,7 +5,7 @@ xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwi
 echo 'Testing OptimizelySwiftSDK-iOS (iPhone SE,OS=10.0)'
 xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-iOS -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone SE,OS=10.0' test
 echo 'Testing OptimizelySwiftSDK-iOS (iPhone XS,OS=12.1)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-iOS -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XS,OS=12.1' test
+xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-iOS -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XS,OS=13.1' test
 
 echo 'Testing OptimizelySwiftSDK-tvOS (Apple TV 1080p,OS=9.0)'
 xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=9.0' test
@@ -14,4 +14,4 @@ xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwi
 echo 'Testing OptimizelySwiftSDK-tvOS (Apple TV,OS=12.1)'
 xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV,OS=12.1' test
 echo 'Testing OptimizelySwiftSDK-tvOS (Apple TV 4K,OS=12.1)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV 4K,OS=12.1' test
+xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV 4K,OS=13.0' test

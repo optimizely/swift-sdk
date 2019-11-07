c707f7ae67c050adf86cf503ad585b02d5b3cfac
Jae Kim <jae.kim@optimizely.com>
2019-11-07 13:19:37 -0800

    fix start_simulator script to filter out minor iOS version of simulator

--- Scripts/start_simulator.sh
+++ Scripts/start_simulator.sh
@@ -7,7 +7,9 @@ set -eou pipefail
 # NAME (eg. iPad Air)
 
 # prep jq arg because it doesnt allow parameter expansion within its single quotes
-echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${OS/./-}\"" > /tmp/jq_file
+vs=( ${OS//./ } )                   # replace points, split into array
+version="${vs[0]}-${vs[1]}"         # truncate minor version of iOS (simctl list does not differentiate minor versions: 10.3.1 -> 10-3)
+echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${version}\"" > /tmp/jq_file
 
 # print out all available simulator versions in travis xcode
 #xcrun simctl list

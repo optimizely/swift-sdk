82ef4c9a535c13c06ce8c4df77458f644ce55b67
Jae Kim <jae.kim@optimizely.com>
2021-06-24 13:36:34 -0700

    limit code coverage to OptimizelySwift target only

--- Scripts/start_simulator.sh
+++ Scripts/start_simulator.sh
@@ -12,7 +12,8 @@ echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${OS
 simulator=$( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' )
 if [ -z $simulator ]; then
     echo "The requested simulator ($PLATFORM $OS $NAME) cannot be found."
-    xcrun instruments -s device
+    #xcrun instruments -s device
+    xcrun xctrace list devices
     exit 1
 fi
 

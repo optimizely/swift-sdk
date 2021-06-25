cf943ebb7cccbb88243406c7ac893a97461bc0fe
Jae Kim <jae.kim@optimizely.com>
2021-06-25 15:04:11 -0700

    fix error in script

--- Scripts/upload_coveralls.sh
+++ Scripts/upload_coveralls.sh
@@ -10,11 +10,12 @@
 # - exclude coverage for Test codes by setting OptimizelySwiftSDK-iOS scheme > Test > Options > Gather coverage for selected targets
 # - report coverage for PR and iPhone 11 only (avoid redundant ones)
 # - use Xcode12.4+ (older Xcode reports a wrong number)
-if [[ "$TRAVIS_BRANCH" == "master" && "$PLATFORM" == "iOS Simulator" && "$NAME" == "iPhone 11" ]]; then Scripts/upload_coveralls.sh; fi
-        
-mkdir xccov2lcov && cd xccov2lcov && git init && git fetch --depth=1 https://github.com/trax-retail/xccov2lcov.git && git checkout FETCH_HEAD
-xcrun xccov view --report --json ../$COVERAGE_DIR/Logs/Test/*.xcresult  > coverage.json
-swift run xccov2lcov coverage.json > lcov.info
+if [[ "$TRAVIS_BRANCH" == "master" && "$PLATFORM" == "iOS Simulator" && "$NAME" == "iPhone 11" ]]
+then
+  mkdir xccov2lcov && cd xccov2lcov && git init && git fetch --depth=1 https://github.com/trax-retail/xccov2lcov.git && git checkout FETCH_HEAD
+  xcrun xccov view --report --json ../$COVERAGE_DIR/Logs/Test/*.xcresult  > coverage.json
+  swift run xccov2lcov coverage.json > lcov.info
 
-cd ..
-coveralls-lcov -v --repo-token $COVERALLS_TOKEN   xccov2lcov/lcov.info
+  cd ..
+  coveralls-lcov -v --repo-token $COVERALLS_TOKEN   xccov2lcov/lcov.info
+fi

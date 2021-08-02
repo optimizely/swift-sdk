05d0a338b76f8c8135e2f147d4f770af8844b1d3
Jae Kim <jae.kim@optimizely.com>
2021-08-02 09:56:34 -0700

    cleanup tests

--- Tests/OptimizelyTests-MultiClients/ConcurrencyTests_SingleClient.swift
+++ Tests/OptimizelyTests-MultiClients/ConcurrencyTests_SingleClient.swift
@@ -18,6 +18,11 @@ import XCTest
 
 class ConcurrencyTests_SingleClient: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        HandlerRegistryService.shared.binders.property?.removeAll()
+    }
+
     func testDatafileUpdateConcurrent() {
         let optimizely = OptimizelyClient(sdkKey: OTUtils.randomSdkKey)
         let datafile = OTUtils.loadJSONDatafile("empty_traffic_allocation")!
@@ -39,29 +44,6 @@ class ConcurrencyTests_SingleClient: XCTestCase {
         XCTAssertTrue(result, "Concurrent tasks timed out")
     }
     
-    func testDatafileUpdateConcurrent_2() {
-        let optimizely = OptimizelyClient(sdkKey: OTUtils.randomSdkKey)
-        let datafile = OTUtils.loadJSONDatafile("empty_traffic_allocation")!
-        try! optimizely.start(datafile: datafile)
-        
-        let result = OTUtils.runConcurrent(count: 10, timeoutInSecs: 60) { idx in
-            for _ in 0..<100 {
-                let config = try! ProjectConfig(datafile: datafile)
-                optimizely.config = config
-                
-                let logger = OPTLoggerFactory.getLogger()
-                logger.e("e-level message")
-
-                // verify log call not conflicted with concurrent config update
-                _ = optimizely.isFeatureEnabled(featureKey: "feature_1", userId: "tester")
-            }
-            
-            print("Testing: testDatafileUpdateConcurrent_2: \(idx)")
-        }
-        
-        XCTAssertTrue(result, "Concurrent tasks timed out")
-    }
-        
     func testLoggerCreatedProperlyOnClientInitialization() {
         let optimizely = OptimizelyClient(sdkKey: OTUtils.randomSdkKey,
                                           logger: MockLogger())
@@ -70,7 +52,11 @@ class ConcurrencyTests_SingleClient: XCTestCase {
         
         let message = OptimizelyError.invalidJSONVariable
         MockLogger.expectedLog = message.localizedDescription
-        
+      
+        MockLogger.logFound = false
+        optimizely.logger.i(message)
+        XCTAssert(MockLogger.logFound)
+
         MockLogger.logFound = false
         (optimizely.datafileHandler as! DefaultDatafileHandler).logger.i(message)
         XCTAssert(MockLogger.logFound)

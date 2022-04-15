fff7e6f2b08e250da33683d563df57ba33c219a4
Yasir Ali <yali@folio3.com>
2022-04-15 16:30:01 +0500

    clean up.

--- Scripts/start_simulator.sh
+++ Scripts/start_simulator.sh
@@ -9,21 +9,21 @@ set -eou pipefail
 # More about XCode and its compatible simulators can be found here: https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md
 sudo mkdir -p /Library/Developer/CoreSimulator/Profiles/Runtimes
 
-if [ $SIMULATOR_XCODE != 12.4 ]; then
+if [ "$SIMULATOR_XCODE" != 12.4 ]; then
 
     os_folder="iPhoneOS"
     os="${OS/./-}"
     name="${NAME// /-}"
 
     # Check if device is Apple tv, update os_folder for linking purposes
-    if [ $NAME == "Apple TV 4K" ]
+    if [ "$NAME" == "Apple TV 4K" ]
     then
         name="${name}-4K"
         os_folder="AppleTVOS"
     fi
 
     # update os_folder as per xcode version
-    if [ $SIMULATOR_XCODE == 10.3 ]
+    if [ "$SIMULATOR_XCODE" == 10.3 ]
     then
         os_folder="${os_folder}.platform/Developer/Library"
     else

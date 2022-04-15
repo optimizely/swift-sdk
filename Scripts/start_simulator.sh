c26f22d350ef50f81a1e12276c3dd8ee2dc3a24b
Yasir Ali <yali@folio3.com>
2022-04-15 16:08:43 +0500

    cleanup.

--- Scripts/start_simulator.sh
+++ Scripts/start_simulator.sh
@@ -2,40 +2,41 @@
 set -eou pipefail
 
 # expects the following environment variables defined
-# PLATFORM (eg. iOS Simulator)
+# SIMULATOR_XCODE (Which Xcode's simulator to use)
 # OS (eg. 12.0)
 # NAME (eg. iPad Air)
+# OS_TYPE (eg. iOS)
 
 # prep jq arg because it doesnt allow parameter expansion within its single quotes
-echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${OS/./-}\"" > /tmp/jq_file
 sudo mkdir -p /Library/Developer/CoreSimulator/Profiles/Runtimes
-if [ "$SIMULATOR_XCODE" == 10.3 ]
-then
-    os_folder="iPhoneOS"
-    if [ "$NAME" == "Apple TV 4K" ]
-    then
-        os_folder="AppleTVOS"
-    fi
-    sudo ln -s /Applications/Xcode_10.3.app/Contents/Developer/Platforms/$os_folder.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/$OS_TYPE.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE\ $OS.simruntime
-elif [ "$SIMULATOR_XCODE" != 12.4 ]; then 
-    sudo ln -s /Applications/Xcode_$SIMULATOR_XCODE.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE\ $OS.simruntime
-fi
 
-if [ "$SIMULATOR_XCODE" != 12.4 ]
-then
+if [ "$SIMULATOR_XCODE" != 12.4 ]; then
+
+    os_folder="iPhoneOS"
     os="${OS/./-}"
     name="${NAME// /-}"
-    if [ "$name" == "Apple-TV-4K" ]
+
+    # Check if device is Apple tv, update os_folder for linking purposes
+    if [ "$NAME" == "Apple TV 4K" ]
     then
         name="${name}-4K"
+        os_folder="AppleTVOS"
     fi
-    
+
+    # update os_folder as per xcode version
+    if [ "$SIMULATOR_XCODE" == 10.3 ]
+    then
+        os_folder="${os_folder}.platform/Developer"
+    else
+        os_folder="${os_folder}.platform"
+    fi
+
+    # Create link and create simulators which are not part of the current xcode version
+    sudo ln -s /Applications/Xcode_$SIMULATOR_XCODE.app/Contents/Developer/Platforms/$os_folder/Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE\ $OS.simruntime
     xcrun simctl create "custom-device" "com.apple.CoreSimulator.SimDeviceType.$name" "com.apple.CoreSimulator.SimRuntime.$OS_TYPE-$os"
     CUSTOM_SIMULATOR="$(instruments -s devices | grep -m 1 'custom-device' | awk -F'[][]' '{print $2}')"
 else
     CUSTOM_SIMULATOR=$( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' )
 fi
 
-xcrun simctl list runtimes
 xcrun simctl boot $CUSTOM_SIMULATOR && sleep 30
-xcrun simctl list | grep Booted

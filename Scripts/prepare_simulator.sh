a5cbf0ea8f91daaaa47485842d587124b683258a
Yasir Ali <yali@folio3.com>
2022-04-18 15:50:10 +0500

    testing.

--- Scripts/prepare_simulator.sh
+++ Scripts/prepare_simulator.sh
@@ -19,9 +19,9 @@ if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
     sudo mkdir -p /Library/Developer/CoreSimulator/Profiles/Runtimes
 
     # Check if device is Apple tv, update os_folder for linking purposes
-    if [ "$NAME" == "Apple TV 4K" ]
+    if [[ "$NAME" = "Apple TV"* ]]
     then
-        name="${name}-4K"
+        name="${name}-1080p"
         os_folder="AppleTVOS"
     fi
 
@@ -32,6 +32,9 @@ if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
     else
         os_folder="${os_folder}.platform/Library/Developer"
     fi
+
+    echo "======================="
+    echo $SIMULATOR_XCODE_VERSION
 
     # Link and create simulators from older xcode versions which are not part of the current xcode version
     sudo ln -s /Applications/Xcode_$SIMULATOR_XCODE_VERSION.app/Contents/Developer/Platforms/$os_folder/CoreSimulator/Profiles/Runtimes/$OS_TYPE.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE\ $OS.simruntime

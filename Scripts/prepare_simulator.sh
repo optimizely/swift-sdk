50250d47356d0fbc2d813ffca3f2da09094f50c0
Yasir Ali <yali@folio3.com>
2022-04-18 13:36:20 +0500

    fixing.

--- Scripts/prepare_simulator.sh
+++ Scripts/prepare_simulator.sh
@@ -8,6 +8,7 @@ set -eou pipefail
 # OS_TYPE (eg. iOS)
 # SIMULATOR_XCODE_VERSION (Which Xcode's simulator to use)
 # Since github actions only provide limit simulators with each xcode, we need to link simulators from other versions of xcode to be used by current xcode.
+# We must use old simulators with current xcode since old xcode doesnot support swift 5 required by Swift SDK. 
 # More about XCode and its compatible simulators can be found here: https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md
 
 if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
@@ -16,11 +17,11 @@ if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
     name="${NAME// /-}"
 
     sudo mkdir -p /Library/Developer/CoreSimulator/Profiles/Runtimes
-    
+
     # Check if device is Apple tv, update os_folder for linking purposes
-    if [ "$NAME" == "Apple TV 4K" ]
+    if [[ "$NAME" = "Apple TV"* ]]
     then
-        name="${name}-4K"
+        name="${name}-1080p"
         os_folder="AppleTVOS"
     fi
 
@@ -35,11 +36,10 @@ if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
     # Link and create simulators from older xcode versions which are not part of the current xcode version
     sudo ln -s /Applications/Xcode_$SIMULATOR_XCODE_VERSION.app/Contents/Developer/Platforms/$os_folder/CoreSimulator/Profiles/Runtimes/$OS_TYPE.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE\ $OS.simruntime
     xcrun simctl create "custom-device" "com.apple.CoreSimulator.SimDeviceType.$name" "com.apple.CoreSimulator.SimRuntime.$OS_TYPE-$os"
-    CUSTOM_SIMULATOR="$(instruments -s devices | grep -m 1 'custom-device' | awk -F'[][]' '{print $2}')"
+    UDID="$(instruments -s devices | grep -m 1 'custom-device' | awk -F'[][]' '{print $2}')"
 else
     echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${OS/./-}\"" > /tmp/jq_file
-    CUSTOM_SIMULATOR=$( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' )
+    UDID=$( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' )
 fi
 
-xcrun simctl boot $CUSTOM_SIMULATOR && sleep 30
-xcrun simctl list | grep Booted
+echo "$UDID"
--- Scripts/test_all.sh
+++ Scripts/test_all.sh
@@ -1,13 +1,16 @@
-echo 'Testing OptimizelySwiftSDK-iOS (iPhone 8,OS=12.1)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-iOS -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=12.1' test
-echo 'Testing OptimizelySwiftSDK-iOS (iPhone SE,OS=11.1)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-iOS -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone SE,OS=11.1' test
-echo 'Testing OptimizelySwiftSDK-iOS (iPhone XS,OS=13.2)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-iOS -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XS,OS=13.2' test
+deviceModels=("iPhone SE" "iPhone 8" "iPhone 11", "Apple TV" "Apple TV" "Apple TV 4K")
+osVersions=("12.4" "13.3" "14.4", "12.4", "13.3", "14.3")
+xcodeVersions=("10.3" "11.3.1" "12.4", "10.3", "11.3.1", "12.4")
+platforms=("iOS" "iOS" "iOS", "tvOS", "tvOS", "tvOS")
+testSdks=("iphonesimulator" "iphonesimulator" "iphonesimulator", "appletvsimulator", "appletvsimulator", "appletvsimulator")
 
-echo 'Testing OptimizelySwiftSDK-tvOS (Apple TV,OS=11.1)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV,OS=11.1' test
-echo 'Testing OptimizelySwiftSDK-tvOS (Apple TV,OS=12.1)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV,OS=12.1' test
-echo 'Testing OptimizelySwiftSDK-tvOS (Apple TV 4K,OS=13.2)'
-xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme OptimizelySwiftSDK-tvOS -sdk appletvsimulator -destination 'platform=tvOS Simulator,name=Apple TV 4K,OS=13.2' test
+for i in ${!deviceModels[@]}; do
+  PLATFORM="${platforms[$i]} Simulator"
+  OS="${osVersions[$i]}"
+  NAME="${deviceModels[$i]}"
+  OS_TYPE="${platforms[$i]}"
+  SIMULATOR_XCODE_VERSION="${xcodeVersions[$i]}"
+  Scripts/prepare_simulator.sh
+  echo "Testing OptimizelySwiftSDK-${platforms[$i]} (${deviceModels[$i]},OS=${osVersions[$i]})"
+  xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme "OptimizelySwiftSDK-${platforms[$i]}" -sdk "${testSdks[$i]}" -destination "platform=${platforms[$i]} Simulator,name=${deviceModels[$i]},OS=${osVersions[$i]}" test
+done

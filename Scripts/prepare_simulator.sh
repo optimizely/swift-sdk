76f75ade994eb6d845f853599ef114acd483e14f
Yasir Ali <yali@folio3.com>
2022-04-18 19:27:47 +0500

    testing.

--- Scripts/prepare_simulator.sh
+++ Scripts/prepare_simulator.sh
@@ -38,5 +38,17 @@ if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
     xcrun simctl create "custom-device" "com.apple.CoreSimulator.SimDeviceType.$name" "com.apple.CoreSimulator.SimRuntime.$OS_TYPE-$os"
 fi
 
+# prep jq arg because it doesnt allow parameter expansion within its single quotes
 echo ".devices.\"com.apple.CoreSimulator.SimRuntime.${PLATFORM/ Simulator/}-${OS/./-}\"" > /tmp/jq_file
-echo $( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' )
+simulator=$( xcrun simctl list --json devices | jq -f /tmp/jq_file | jq -r '.[] | select(.name==env.NAME) | .udid' )
+xcrun xctrace list devices
+
+if [ -z $simulator ]; then
+    echo "The requested simulator ($PLATFORM $OS $NAME) cannot be found."
+    #xcrun instruments -s device
+    xcrun xctrace list devices
+    sleep 3
+    exit 1
+fi
+
+echo "$simulator"

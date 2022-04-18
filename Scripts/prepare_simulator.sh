f5a6cff23f7028cfc6da60c471717bb6832e2079
Yasir Ali <yali@folio3.com>
2022-04-18 16:41:11 +0500

    testing.

--- Scripts/prepare_simulator.sh
+++ Scripts/prepare_simulator.sh
@@ -33,12 +33,7 @@ if [ "$SIMULATOR_XCODE_VERSION" != 12.4 ]; then
         os_folder="${os_folder}.platform/Library/Developer"
     fi
 
-    printf "\n======================="
-    printf "\nSIMULATOR_XCODE_VERSION: %s" "$SIMULATOR_XCODE_VERSION"
-    printf "\n======================="
-    printf "\nOS: %s" "$OS"
-    printf "\n======================="
-    printf "\nNAME: %s" "$NAME"
+    echo "=======$SIMULATOR_XCODE_VERSION========"
 
     # Link and create simulators from older xcode versions which are not part of the current xcode version
     sudo ln -s /Applications/Xcode_$SIMULATOR_XCODE_VERSION.app/Contents/Developer/Platforms/$os_folder/CoreSimulator/Profiles/Runtimes/$OS_TYPE.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/$OS_TYPE\ $OS.simruntime

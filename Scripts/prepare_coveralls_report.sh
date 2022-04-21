c97248d77267d936c45ba0b68ecdce79a761697e
Yasir Ali <yali@folio3.com>
2022-04-21 11:49:14 +0500

    Testing coveralls.

--- Scripts/prepare_coveralls_report.sh
+++ Scripts/prepare_coveralls_report.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-# upload_coveralls.sh
+# prepare_coveralls_report.sh
 #
 # Usage:
-#  $ ./upload_coveralls.sh
+#  $ ./prepare_coveralls_report.sh
 #
 
 # [coveralls]
@@ -15,7 +15,4 @@ then
   mkdir xccov2lcov && cd xccov2lcov && git init && git fetch --depth=1 https://github.com/trax-retail/xccov2lcov.git && git checkout FETCH_HEAD
   xcrun xccov view --report --json ../$COVERAGE_DIR/Logs/Test/*.xcresult  > coverage.json
   swift run xccov2lcov coverage.json > lcov.info
-
-  cd ..
-  coveralls-lcov -v --repo-token $COVERALLS_TOKEN   xccov2lcov/lcov.info
 fi

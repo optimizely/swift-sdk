f7a9b7947453be2d796b5aa3e4de8913441c448b
Jae Kim <jae.kim@optimizely.com>
2023-04-18 17:09:13 -0700

    clean up local test-all tests

--- Scripts/test_all.sh
+++ Scripts/test_all.sh
@@ -5,19 +5,21 @@
 # More about XCode and its compatible simulators can be found here: https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md
 # https://github.com/actions/virtual-environments/issues/551
 
-deviceModels=("iPhone 11" "iPhone 11" "iPhone 11" "Apple TV")
-osVersions=("13.7" "14.4" "15.0" "16.4")
-#xcodeVersions=("10.3" "11.3.1" "12.4" "10.3" "11.3.1" "12.4")
+deviceModels=("iPhone 12" "iPhone 8" "iPad Air (4th generation)" "Apple TV")
+osVersions=("16.0" "14.4" "15.0" "16.0")
+xcodeVersions=("14.3" "14.3" "14.3" "14.3")
 platforms=("iOS" "iOS" "iOS" "tvOS")
 testSdks=("iphonesimulator" "iphonesimulator" "iphonesimulator" "appletvsimulator")
 
 for i in "${!deviceModels[@]}"; do
-  #export PLATFORM="${platforms[$i]} Simulator"
-  #export OS="${osVersions[$i]}"
-  #export NAME="${deviceModels[$i]}"
-  #export OS_TYPE="${platforms[$i]}"
-  #export SIMULATOR_XCODE_VERSION="${xcodeVersions[$i]}"
+  export PLATFORM="${platforms[$i]} Simulator"
+  export OS="${osVersions[$i]}"
+  export NAME="${deviceModels[$i]}"
+  export OS_TYPE="${platforms[$i]}"
+  export SIMULATOR_XCODE_VERSION="${xcodeVersions[$i]}"
+  
   Scripts/prepare_simulator.sh
   echo "Testing OptimizelySwiftSDK-${platforms[$i]} (${deviceModels[$i]},OS=${osVersions[$i]})"
+  
   xcrun xcodebuild -workspace OptimizelySwiftSDK.xcworkspace -scheme "OptimizelySwiftSDK-${platforms[$i]}" -sdk "${testSdks[$i]}" -destination "platform=${platforms[$i]} Simulator,name=${deviceModels[$i]},OS=${osVersions[$i]}" test
 done

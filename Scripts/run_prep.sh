a5aaa55f9a8f4d2118e1b602ed96ce28ebbc2ef0
Jae Kim <jae.kim@optimizely.com>
2023-04-28 13:10:09 -0700

    fix error

--- Scripts/run_prep.sh
+++ Scripts/run_prep.sh
@@ -87,7 +87,7 @@ function push_changes {
     }
   git push -f https://${GITHUB_TOKEN}@github.com/${REPO_SLUG} ${AUTOBRANCH}
   MESSAGE='[FSSDK-N/A]'  # a dummy ref for required FSSDK checking
-  PR_URL=$(hub pull-request --no-edit -b ${BRANCH} -h ${AUTOBRANCH} -F ${MESSAGE}) 
+  PR_URL=$(hub pull-request --no-edit -b ${BRANCH} -h ${AUTOBRANCH} -m "${MESSAGE}") 
   echo -e "${COLOR_CYAN}ATTENTION:${COLOR_RESET} review and merge ${COLOR_CYAN}${PR_URL}${COLOR_RESET}"
   echo "then to release to cocoapods use Git action's Trigger build with the following payload:"
   echo -e "${COLOR_MAGENTA}env:${COLOR_RESET}"

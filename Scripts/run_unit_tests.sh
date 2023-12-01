cdd7ecfb3c02e5e787aacf4a965995e91d8696b1
Muzahidul Islam <129880873+muzahidul-opti@users.noreply.github.com>
2023-12-01 23:04:17 +0600

    Release 3.10.3 (#525)
    * Version updated
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update README.md
    
    * Update CHANGELOG.md
    
    * Git action allow all PR
    
    * Github action trigger on push to master
    
    * Script updated
    
    * Version and date updated

--- Scripts/run_unit_tests.sh
+++ Scripts/run_unit_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # run xcode unit tests
 #
@@ -7,7 +7,7 @@
 #
 
 # unit tests for PR only
-if [[ "$TRAVIS_BRANCH" == "master" ]]
+if [[ "$BRANCH" == "master" ]]
 then
   xcodebuild test -derivedDataPath $COVERAGE_DIR  -workspace OptimizelySwiftSDK.xcworkspace -scheme $SCHEME -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -sdk $TEST_SDK -destination "platform=$PLATFORM,OS=$OS,name=$NAME" ONLY_ACTIVE_ARCH=YES | tee buildoutput | xcpretty && test ${PIPESTATUS[0]} -eq 0
 fi

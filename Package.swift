06d165f96d563ce6b9ddcbd209bb4f2fe30f1c86
Charly Liu Chou <charly.liu@adevinta.com>
2024-01-17 16:12:50 +0100

    Add PrivacyInfo as resource in the bundle

--- Package.swift
+++ Package.swift
@@ -14,7 +14,11 @@ let package = Package(
                  targets: ["Optimizely"])
     ],
     targets: [
-        .target(name: "Optimizely", path: "Sources")
+        .target(name: "Optimizely", 
+                path: "Sources",
+                exclude: ["Supporting Files/PrivacyInfo.xcprivacy"],
+                resources: [.copy("Supporting Files/PrivacyInfo.xcprivacy")]
+        )
     ],
     swiftLanguageVersions: [.v5]
 )

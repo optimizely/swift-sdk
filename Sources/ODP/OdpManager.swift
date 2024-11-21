8c616833354878c8ebeeb8dc3f19a14714add40e
Muzahidul Islam <muzahidul.islam@Optimizely.com>
2024-11-21 07:59:33 +0600

    fix: intialize event send by odpmanager

--- Sources/ODP/OdpManager.swift
+++ Sources/ODP/OdpManager.swift
@@ -61,6 +61,9 @@ public class OdpManager {
         self.odpConfig = OdpConfig()
         self.segmentManager.odpConfig = odpConfig
         self.eventManager.odpConfig = odpConfig
+        if let vuid = vuid, VuidManager.isVuid(vuid) {
+            self.eventManager.sendInitializedEvent(vuid: vuid)
+        }
     }
     
     func fetchQualifiedSegments(userId: String,
